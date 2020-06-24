098ea0db8faaa49b7ee2cfc23a1fd2148eda9a02
60083638 <Darya.Gritsienko@leroymerlin.ru>
2020-06-24 23:09:06 +0300

    Личный проект: валидируй, как никогда не валидировал

--- src/index.js
+++ src/index.js
@@ -5,7 +5,11 @@ import App from './components/App';
 const filmsData = {
   'genres': [`Drama`],
   'years': [`2014`],
-  'titles': [`The Grand Budapest Hotel`]
+  'titles': [`The Grand Budapest Hotel`, `Fantastic Beasts: The Crimes of Grindelwald`, `Bohemian Rhapsody`,
+    `Macbeth`, `Aviator`, `We need to talk about Kevin`, `What We Do in the Shadows`, `Revenant`, `Johnny English`,
+    `Shutter Island`, `Pulp Fiction`, `No Country for Old Men`, `Snatch`, `Moonrise Kingdom`, `Seven Years in Tibet`,
+    `Midnight Special`, `War of the Worlds`, `Dardjeeling Limited`, `Orlando`, `Mindhunter`, `Midnight Special`
+  ]
 };
 
 ReactDOM.render(
